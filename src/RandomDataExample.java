0d2a342dd77aec5d794970c0339ec082c5cefed7
Jose Gramajo <gra22907@uvg.edu.gt>
2023-02-28 10:08:23 -0600

    Hashmap

--- src/RandomDataExample.java
+++ src/RandomDataExample.java
@@ -3,11 +3,23 @@ import java.util.*;
 public class RandomDataExample {
 
     public static void main(String[] args) {
-    // Generate 100 random data
+        // Generate 100 random data
         Random random = new Random();
         int[] dataArray = new int[100];
         for (int i = 0; i < 100; i++) {
             dataArray[i] = random.nextInt(1000);
+        }
+
+        // Use HashMap to count the number of occurrences of each piece of data
+        Map<Integer, Integer> hashMap = new HashMap<>();
+        for (int i = 0; i < 100; i++) {
+            int data = dataArray[i];
+            if (hashMap.containsKey(data)) {
+                int count = hashMap.get(data);
+                hashMap.put(data, count + 1);
+            } else {
+                hashMap.put(data, 1);
+            }
         }
     }
 }
